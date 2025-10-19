db34a1bfe174525cfd5569df9b31b6022add7d49
Todd Cytra <tcytra@gmail.com>
2025-10-19 16:30:47 -0700

    Repository: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Enraiged Exports
+
+**Enraiged Exports provides data exporting feature for Laravel Enraiged projects.**
+
+> **Please Note:** This package is currently a work in progress and not yet ready for production use.
---
+++ src/ExportsServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Enraiged\Exports;
+
+// use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class ExportsServiceProvider extends ServiceProvider
+{
+    /**
+     *  Bootstrap the event services.
+     *
+     *  @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
